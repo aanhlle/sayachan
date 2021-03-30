710bee01389943fd5c4efdf430e4deff15509565
aanhlle <levietanh1112@gmail.com>
2021-03-30 22:13:26 +0700

    remove config.json code line

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const fs = require("fs");
 const Discord = require("discord.js");
-// const { prefix, token } = require("./config.json");
 const prefix = "!";
 
 const client = new Discord.Client();
