7a584706f8105793f32970a07e691e92f21ec52d
aanhlle <levietanh1112@gmail.com>
2021-04-17 21:57:03 +0700

    change !ping cmd to !jiko

--- commands/message/jiko.js
+++ commands/message/jiko.js
@@ -1,6 +1,6 @@
 module.exports = {
-  name: "ping",
-  description: "Ping!",
+  name: "jiko",
+  description: "Jikoshoukai!",
   cooldown: 5,
   execute(message) {
     message.channel.send("Xin chào! (✿✪‿✪｡)ﾉｺﾝﾁｬ♡. Hãy giới thiệu bản thân bạn tại #自己紹介ーgiới-thiệu-bản-thân nhé!");
